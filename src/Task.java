15c12a835aa1340eddf6e6fbec12d5db3b96630b
Nadezhda Bizina <nadezhda.bizina1433@gmail.com>
2023-03-29 01:01:30 +0300

    add TaskCreator() method

--- src/Task.java
+++ src/Task.java
@@ -1,4 +1,8 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class Task {
 
@@ -11,10 +15,12 @@ public class Task {
   private boolean importance;
   private boolean urgent;
   private String status;
+  private int importanceInt;
 
 
-  public Task(LocalDate dateOfCreate, String taskName, String taskDescription, boolean importance, boolean importance1, boolean urgent, String status) {
-    this.dateOfCreate = dateOfCreate;
+  public Task(LocalDateTime dateOfCreate, String taskName, String taskDescription,
+              boolean importance, boolean urgent, String status) {
+    this.dateOfCreate = LocalDate.from(dateOfCreate);
     this.taskName = taskName;
     this.taskDescription = taskDescription;
     this.status = status;
@@ -22,7 +28,8 @@ public class Task {
     this.urgent = false;
   }
 
-  public Task(LocalDate dateOfCreate, String taskName, boolean importance, boolean importance1, boolean urgent, String status) {
+  public Task(LocalDate dateOfCreate, String taskName,
+              boolean importance, boolean urgent, String status) {
     this.dateOfCreate = dateOfCreate;
     this.taskName = taskName;
     this.status = status;
@@ -53,6 +60,7 @@ public class Task {
   public String getStatus() {
     return status;
   }
+
   public void setTaskName(String taskName) {
     this.taskName = taskName;
   }
@@ -71,5 +79,87 @@ public class Task {
 
   public void setStatus(String status) {
     this.status = status;
+  }
+
+  public Task TaskCreator() throws IOException {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    LocalDateTime dateOfCreate = LocalDateTime.now();
+
+    System.out.print("Введите название задачи: ");
+    String taskName = br.readLine();
+
+    System.out.print("Введите описание задачи: ");
+    String taskDescription = br.readLine();
+
+    System.out.printf("Введите: %n'1' если задача важная %n'2' если задача не важная%n");
+    int importanceInt = Integer.parseInt(br.readLine());
+    boolean a = false;
+    boolean importance = false;
+    do {
+      if (importanceInt == 1) {
+        importance = true;
+        a = true;
+      } else {
+        if (importanceInt == 2) {
+          a = true;
+        } else {
+          System.out.println("Введите: %n'1' если задача важная %n'2' если задача не важная%n");
+        }
+      }
+    } while (!a);
+
+    System.out.printf("Введите: %n'1' если задача срочная %n'2' если задача не срочная%n");
+    int urgentInt = Integer.parseInt(br.readLine());
+    boolean b = false;
+    boolean urgent = false;
+    do {
+      if (urgentInt == 1) {
+        urgent = true;
+        b = true;
+      } else {
+        if (urgentInt == 2) {
+          b = true;
+        } else {
+          System.out.println("Введите: %n'1' если задача срочная %n'2' если задача не срочная%n");
+        }
+      }
+    } while (!b);
+
+    System.out.printf("Введите: %n'1' если задача не начата (status 'to do')" +
+        "%n'2' если задача выполняется (status 'in process')" +
+        "%n'3' если задача завершена (status 'done')%n");
+    int statusInt = Integer.parseInt(br.readLine());
+    boolean c = false;
+    String status = null;
+    do {
+      if (statusInt == 1) {
+        status = "to do";
+        c = true;
+      } else {
+        if (statusInt == 2) {
+          status = "in process";
+          c = true;
+        } else {
+          if (statusInt == 3) {
+            status = "done";
+            c = true;
+          } else {
+            System.out.println("Введите: %n'1' если задача не начата (status 'to do')" +
+                "%n'2' если задача выполняется (status 'in process')" +
+                "%n'3' если задача завершена (status 'done')");
+          }
+        }
+      }
+    } while (!c);
+
+
+    return new Task(dateOfCreate,
+        taskName,
+        taskDescription,
+        importance,
+        urgent,
+        status);
   }
 }
