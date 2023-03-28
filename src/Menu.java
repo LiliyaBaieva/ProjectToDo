3028a08331a3e2e7282ae226eefd50c5347af669
Valentyna Kondrashenko <oreshkav@gmail.com>
2023-03-28 22:23:22 +0200

    add menuList(), menuListSmall()

--- src/Menu.java
+++ src/Menu.java
@@ -18,15 +18,22 @@ public class Menu {
   public static void main(String[] args) throws IOException {
     System.out.println("\nMENU");
     List<String> menuMain = menuList();
+
+    // вывод подменю критериев отбора
+    if ( readMenu(menuMain).equals("3")) {
+      System.out.println("Выберите критерий отбора: ");
+      menuListSmall();
+      System.out.printf("Введите номер от 1 до %d: ", menuMain.size());
+    }
   }
 
   public static List<String> menuList() {
     List<String> menuMain = new ArrayList<>();
-    menuMain.add("- добавить запись.");
-    menuMain.add("- изменить запись.");
-    menuMain.add("- вывести задачи на экран.");
-    menuMain.add("- удалить запись.");
-    menuMain.add("- выйти.");
+    menuMain.add("Добавить запись.");
+    menuMain.add("Изменить запись.");
+    menuMain.add("Вывести задачи на экран.");
+    menuMain.add("Удалить запись.");
+    menuMain.add("Выйти.");
 
     for (int i = 0; i < menuMain.size(); ++i) {
       System.out.println(((i + 1) + ". " + menuMain.get(i)));
@@ -39,7 +46,7 @@ public class Menu {
     String menuNumber = "";
     int num = 0;
     while (num < 1 || num > menuMain.size()) {
-      System.out.printf("Выберите действие и введите номер от 1 до %d", menuMain.size());
+      System.out.printf("Выберите действие и введите номер от 1 до %d:  -  ", menuMain.size());
       menuNumber = br.readLine();
       try {
         num = Integer.parseInt(menuNumber);
@@ -50,7 +57,19 @@ public class Menu {
     return menuNumber;
   }
 
+  public static List<String> menuListSmall() {
+    List<String> menuSmall = new ArrayList<>();
+    menuSmall.add("Все записи.");
+    menuSmall.add("Выполняются.");
+    menuSmall.add("Ожидают выполнения.");
+    menuSmall.add("Сделаны.");
+    menuSmall.add("Вернуться в главное меню.");
 
+    for (int i = 0; i < menuSmall.size(); ++i) {
+      System.out.println(((i + 1) + ". " + menuSmall.get(i)));
+    }
+    return menuSmall;
+  }
 
 
 
