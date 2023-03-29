1d5486df46af71208af520f198784f1594478f1e
Liliia Baieva <555blv@gmail.com>
2023-03-29 19:46:00 +0300

    refactor TaskUrgentComparator, TaskImportanceComparator

--- src/TaskImportanceComparator.java
+++ src/TaskImportanceComparator.java
@@ -1,11 +1,11 @@
 import java.util.Comparator;
 
-public class TaskImportanceComparator {
+public class TaskImportanceComparator implements Comparator<Task> {
 
   // сортировка по важности
   @Override
   public int compare(Task o1, Task o2) {
-    return o2.getUrgent().compareTo(o1.getUrgent());
+    return o2.getImportance().compareTo(o1.getImportance());
   }
 
 
--- src/TaskUrgentComparator.java
+++ src/TaskUrgentComparator.java
@@ -1,10 +1,10 @@
 import java.util.Comparator;
 
-public class TaskUrgentComparator implements Comparator<Task> {
+public class TaskUrgentComparator implements Comparator<Task>{
 
-  // Сортировка по срочности
+//   Сортировка по срочности
   @Override
   public int compare(Task o1, Task o2) {
-    return o2.getImportance().compareTo(o1.getImportance());
+    return o2.getUrgent().compareTo(o1.getUrgent());
   }
 }
