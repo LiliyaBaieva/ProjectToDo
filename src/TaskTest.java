054ed4a8d85d725c18586ceca2e25410ed7db149
Liliia Baieva <555blv@gmail.com>
2023-03-30 21:00:58 +0300

    add class TaskTest, class TaskDateNameComparatorTests

--- src/TaskTest.java
+++ src/TaskTest.java
@@ -1,6 +1,16 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 public class TaskTest {
+  // Проверка, поле имя не пустое
+  // Проверка, поле описание не пустое
+  // Проверка, поле urgent что возвращает "срочная" или "не срочная"
+  // Проверка, поле importance что возвращает "важная" или "не важная"
+  // сортировки проверить
+
+//  @Test
+
 
 }
