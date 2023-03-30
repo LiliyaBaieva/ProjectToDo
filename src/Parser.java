a0a9a47f11b05edfe8ea53be997411061b857605
Valentyna Kondrashenko <oreshkav@gmail.com>
2023-03-30 14:30:29 +0200

    add fileExist to Parser

--- src/Parser.java
+++ src/Parser.java
@@ -1,53 +1,58 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.time.LocalDate;
 import java.util.*;
 
 public class Parser {
 
 //  public static void main(String[] args) throws IOException {
-//    Parser.parcing();
+//    Parser.parser();
 //  }
+
   final public static String SEP = "; ";
 
-  public static List<Task> parcing() throws IOException {
-    BufferedReader br = new BufferedReader(new FileReader("res/toDoFile.txt"));
+  public static List<Task> parser() throws IOException {
 
-    List<Task> listToDo = new ArrayList<>();
+      File toDoFile = new File("res/toDoFile.txt");
+      if (!toDoFile.exists()) {
+        System.out.println("Файл не найден. Создайте задачи и потом прочитайте.");
+        Menu.menuStart();
+      }
 
-    for (String line = br.readLine(); line != null; line = br.readLine()) {
-      int lastSep = line.indexOf(SEP);
+      BufferedReader br = new BufferedReader(new FileReader("res/toDoFile.txt"));
+      List<Task> listToDo = new ArrayList<>();
 
-      LocalDate dateOfCreate = LocalDate.parse(line.substring(0, lastSep));
-      line = line.substring(lastSep + 2);
-      lastSep = line.indexOf(SEP);
+      for (String line = br.readLine(); line != null; line = br.readLine()) {
+        int lastSep = line.indexOf(SEP);
 
-      String taskName = line.substring(0, lastSep);
-      line = line.substring(lastSep + 2);
-      lastSep = line.indexOf(SEP);
+        LocalDate dateOfCreate = LocalDate.parse(line.substring(0, lastSep));
+        line = line.substring(lastSep + 2);
+        lastSep = line.indexOf(SEP);
 
-      String taskDescription = line.substring(0, lastSep);
-      line = line.substring(lastSep + 2);
-      lastSep = line.indexOf(SEP);
+        String taskName = line.substring(0, lastSep);
+        line = line.substring(lastSep + 2);
+        lastSep = line.indexOf(SEP);
 
-      Boolean importance = Boolean.valueOf(line.substring(0, lastSep));
-      line = line.substring(lastSep + 2);
-      lastSep = line.indexOf(SEP);
+        String taskDescription = line.substring(0, lastSep);
+        line = line.substring(lastSep + 2);
+        lastSep = line.indexOf(SEP);
 
-      Boolean urgent = Boolean.valueOf(line.substring(0, lastSep));
-      String status = line.substring(lastSep + 2);
+        Boolean importance = Boolean.valueOf(line.substring(0, lastSep));
+        line = line.substring(lastSep + 2);
+        lastSep = line.indexOf(SEP);
 
-      Task readedTask =  new Task(dateOfCreate, taskName, taskDescription, importance,
-          urgent, status);
-      listToDo.add(readedTask);
+        Boolean urgent = Boolean.valueOf(line.substring(0, lastSep));
+        String status = line.substring(lastSep + 2);
 
-      System.out.println(readedTask);
-    }
+        Task readedTask = new Task(dateOfCreate, taskName, taskDescription, importance,
+            urgent, status);
+        listToDo.add(readedTask);
+
+//      System.out.println(readedTask);
+      }
 //    System.out.println(listToDo);
 
-    br.close();
+      br.close();
 
-    return listToDo;
+      return listToDo;
+    }
   }
-}
