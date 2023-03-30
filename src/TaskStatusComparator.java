9f6f1045ab642bfe6c1a1e2112b4a524adf24cea
Liliia Baieva <555blv@gmail.com>
2023-03-30 20:58:46 +0300

    remove  class TaskImportanceComparator, TaskStatusComparator.java

--- src/TaskStatusComparator.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class TaskStatusComparator implements Comparator<Task> {
-  // сортировка по статусу
-  @Override
-  public int compare(Task o1, Task o2) {
-    return o1.getStatus().compareTo(o2.getStatus());
-  }
-}
